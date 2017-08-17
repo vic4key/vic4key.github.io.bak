518408cf98e75d93a78d06ba5b713980ccaf075d
vic4key <vic4key@gmail.com>
2017-08-17 11:04:16 +0700

    Sample

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* Main JS File */
+
+$(document).ready(function(){
+	$("#mn_about").click(function() {
+		var home_header  = $(this).text();
+		var home_content = "";
+		$("#md_modal > .modal-dialog > .modal-content > .modal-header > .modal-title").text(home_header);
+		$("#md_modal > .modal-dialog > .modal-content > .modal-body > p").html(home_content);
+	});
+
+	$("#mn_about").click(function() {
+		var about_header  = $(this).text();
+		var about_content = "This is the about content.";
+		$("#md_modal > .modal-dialog > .modal-content > .modal-header > .modal-title").text(about_header);
+		$("#md_modal > .modal-dialog > .modal-content > .modal-body > p").html(about_content);
+	});
+});
