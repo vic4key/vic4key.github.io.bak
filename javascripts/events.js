d7b18417ff790bb431046922aaf6f38c113255bd
Vic P <vic4key@gmail.com>
2019-08-12 02:03:02 +0700

    Sample

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,12 +1,9 @@
-$(document).ready(function(){
-	$("#mn_about").click(function() {
-		var home_header  = $(this).text();
-		var home_content = "123";
-		$("#md_modal > .modal-dialog > .modal-content > .modal-header > .modal-title").text(home_header);
-		$("#md_modal > .modal-dialog > .modal-content > .modal-body > div").html(home_content);
-	});
+$(document).ready(function()
+{
+	/* $("[data-toggle=\"tooltip\"]").tooltip(); */
 
-	$("#mn_about").click(function() {
+	$("#mn_about").click(function()
+	{
 		var about_header  = $(this).text();
 		var about_content = $("#footer * span").text();
 		$("#md_modal > .modal-dialog > .modal-content > .modal-header > .modal-title").text(about_header);
