ddae83829285e0869021463821ee9ef10073fd09
Vic P <vic4key@gmail.com>
2019-08-12 01:42:58 +0700

    Sample

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 	$("#mn_about").click(function() {
 		var home_header  = $(this).text();
-		var home_content = "";
+		var home_content = "123";
 		$("#md_modal > .modal-dialog > .modal-content > .modal-header > .modal-title").text(home_header);
-		$("#md_modal > .modal-dialog > .modal-content > .modal-body > p").html(home_content);
+		$("#md_modal > .modal-dialog > .modal-content > .modal-body > div").html(home_content);
 	});
 
 	$("#mn_about").click(function() {
 		var about_header  = $(this).text();
-		var about_content = "This is the about content.";
+		var about_content = $("#footer * span").text();
 		$("#md_modal > .modal-dialog > .modal-content > .modal-header > .modal-title").text(about_header);
-		$("#md_modal > .modal-dialog > .modal-content > .modal-body > p").html(about_content);
+		$("#md_modal > .modal-dialog > .modal-content > .modal-body").html(about_content);
 	});
 });
